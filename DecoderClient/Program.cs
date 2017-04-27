05721d439568217a1f6fa587a3f2264cd565d4a3
Ilvir <deedpay@gmail.com>
2017-04-27 12:14:31 +0300

    client

--- DecoderClient/Program.cs
+++ DecoderClient/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DecoderClient
 {
     class Program
     {
+        const int port = 8888;
+        const string address = "192.168.8.182";
         static void Main(string[] args)
         {
+            Console.WriteLine("Start message with d: or e: for decrypting or encrypting your message");
+         
+            TcpClient client = null;
+            try
+            {
+                client = new TcpClient(address, port);
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    // ввод сообщения
+                    string message = Console.ReadLine();
+
+                    // преобразуем сообщение в массив байтов
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    // отправка сообщения
+                    stream.Write(data, 0, data.Length);
+
+                    // получаем ответ
+                    data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    message = builder.ToString();
+                    Console.WriteLine("Server: {0}", message);
+                }
+            }
+           
+            finally
+            {
+                client.Close();
+            }
         }
     }
 }
